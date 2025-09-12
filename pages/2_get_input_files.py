e2fb4440709e7e89747ec1c16b29f1b3e9f38e24
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-09-12 14:20:15 +0100

    Update 2_get_input_files.py
    fixing error

--- pages/2_get_input_files.py
+++ pages/2_get_input_files.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Optional, Tuple
 import pandas as pd
 import numpy as np
 import streamlit as st
